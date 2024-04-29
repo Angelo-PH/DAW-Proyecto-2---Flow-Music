75a7b656cadd093d46fd039e73ae501db2e05258
Angelo Ponte <angelo.ponte.h@gmail.com>
2024-04-29 10:53:12 +0200

    Cambios desde windows server

--- public/sql/flowmusic_bd.sql
+++ public/sql/flowmusic_bd.sql
@@ -103,27 +103,6 @@ CREATE TABLE `usuario` (
   `cvv` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Volcado de datos para la tabla `usuario`
---
-
-INSERT INTO `usuario` (`usuario_id`, `usuario_nombre`, `contrasena`, `fecha_registro`, `suscripcion`, `correo_electronico`, `titular_tarjeta_nom`, `tarjeta_num`, `fecha_caducidad`, `cvv`) VALUES
-(3, 'asdasdas', '$2y$10$GQHoJ2uZ/vSM8kY5SCzVkOIePSgVIVtDY8c8D.O8pSbkueaZCEQC2', NULL, NULL, 'asdadas@gmail.com', '', '0', '', 0),
-(4, 'fff', '$2y$10$sUcf3bzxF/EOj3qRm1qXdO9Vvcc4w4lLZVr4sVTA2ju32GICeunw6', NULL, NULL, 'fff@g.n', '', '0', '', 0),
-(5, 'xcv', '$2y$10$3GMqvyB1fX1xBL9Rood84.Ujnhx6XlpcTJyCEQi7Pn54AspJUlglq', NULL, NULL, 'xcv@f.f', '', '0', '', 0),
-(6, 'hola', '$2y$10$oU6cSoha67NsC2QSlAQ8ru2UG/6wNQTBbR/VSx9tC1/LqcoLzjWkm', NULL, NULL, 'hola@hola.php', '', '0', '', 0),
-(7, 'xcv', '$2y$10$MuMSX4qsQTghpBpYGBOJse/ifaaMjl7nvcFqbnA6r/mJNcdneLGIu', NULL, NULL, 'xcv@f.f', '', '0', '', 0),
-(8, 'adam iglesias', '$2y$10$HI3ijUgrE2EoAHvr7GXnAOx0nBPVeT1EeSMxARfgfaUUV8eL488VC', NULL, NULL, 'aiglesias@gmail.com', '', '0', '', 0),
-(9, 'angelo', '$2y$10$hJ.FaFDVjtaKt9WsMflHvuRJ.1CY8TM1puyjbzivdZpjUPeWgOjJe', NULL, NULL, 'angelo@gmail.com', '', '0', '', 0),
-(10, 'angelo ponte', '$2y$10$eMgSI/IAVeiCQbL1J0J2UOkKoxIagNGpHfYRCct5RUqbqRJhCppc2', NULL, NULL, 'angeloponte@gmail.com', '', '0', '', 0),
-(11, 'Registro Ejemplo', '$2y$10$PiGMO8R/Q2pvL0sxlm5q2ebNbeyiTe.Ul9Z7slDjX0a0Y0ttUWMlu', NULL, NULL, 'registroejemplo@gmail.com', '', '0', '', 0),
-(12, 'Registro Ejemplo2 ', '$2y$10$E4fRS.BiPV70ORxsRLo9Veuza3UKsX25AKRfrP9Il887yct.TYqwq', NULL, NULL, 'registroejemplo2@gmail.com', '', '0', '', 0),
-(13, '', '$2y$10$LbUchX/ykjhmVumB8QbHa.hhtkT9EtuVPhXOvPyZAfQSHvTk96xKK', NULL, NULL, '', '', '0', '', 0),
-(14, '', '$2y$10$Mn5J8V12aeeVjNiewVimdOrXXOjqCWY9we6D2Iyy40FnYk6PXbGJO', NULL, NULL, '', '', '0', '', 0),
-(15, '', '$2y$10$i4lBhqSrG1aiCToT1.lohe3BKaMuDSPoySEDB010ygTlO0PtZlAHO', NULL, NULL, '', '', '0', '', 0),
-(16, 'prueba1', '$2y$10$fMgpdEPGSa6B.28PDmng3u92WTLir8mylioIa60Nj2YrXZd6cnaAG', NULL, NULL, 'prueba1@gmail.com', '', '0', '', 0),
-(17, 'xpalau', '$2y$10$DU5x9rKWCp87WJCJKfaVquenKcyJbpmd/ebaQzJaZKtMb1XDggTdy', NULL, 'mensual', 'xpalau@gmail.com', 'Angelo Ponte Hinostroza', '123123123', '123', 123),
-(19, 'usuario prueba', '$2y$10$PDtxpjEK5ALAaJ1kuh4J3eS/gS.UYNJfr30FrnRI2nn25JX/71axS', NULL, 'mensual', 'uprueba@gmail.com', '', '0', '', 0);
 
 --
 -- Índices para tablas volcadas
